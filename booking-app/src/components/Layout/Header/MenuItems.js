126aea35bffafb267858408e75db10f685bad8d7
Pratik Bhagat <pbhagat@deloitte.com>
2020-09-28 15:19:19 +0530

    Dynamic menu items

--- booking-app/src/components/Layout/Header/MenuItems.js
+++ booking-app/src/components/Layout/Header/MenuItems.js
@@ -3,16 +3,28 @@ import Aux from 'react-aux';
 import MenuItem from './MenuItem/MenuItem';
 
 const menuItems = (props) => {
+    let menuItems = [
+        { link: '/car-rentals', label: 'Car Rentals' },
+        { link: '/deals', label: 'Deals' },
+        { link: '/bookings', label: 'Bookings' },
+        { link: '/contact-cs', label: 'Contact Customer Service' },
+        { link: '/book-airport-taxi', label: 'Book Airport Taxi' },
+        { link: '/wishlist', label: 'Wishlist' },
+        { link: '/travel-communities', label: 'Travel Communities' },
+        { link: '/travel-articles', label: 'Travel Articles' }
+    ];
     return (
         <Aux>
-            <MenuItem link="/car-rentals" open={props.open} toggle={props.toggle}>Car Rentals</MenuItem>
-            <MenuItem link="/deals" open={props.open} toggle={props.toggle}>Deals</MenuItem>
-            <MenuItem link="/bookings" open={props.open} toggle={props.toggle}>Bookings</MenuItem>
-            <MenuItem link="/contact-cs" open={props.open} toggle={props.toggle}>Contact Customer Service</MenuItem>
-            <MenuItem link="/book-airport-taxi" open={props.open} toggle={props.toggle}>Book Airport Taxi</MenuItem>
-            <MenuItem link="/wishlist" open={props.open} toggle={props.toggle}>Wishlist</MenuItem>
-            <MenuItem link="/travel-communities" open={props.open} toggle={props.toggle}>Travel Communities</MenuItem>
-            <MenuItem link="/travel-articles" open={props.open} toggle={props.toggle}>Travel Articles</MenuItem>
+            {
+                menuItems.map(item => (
+                    <MenuItem key={item.link}
+                        link={item.link}
+                        open={props.open}
+                        toggle={props.toggle}>
+                        {item.label}
+                    </MenuItem>
+                ))
+            }
         </Aux>
     );
 };
