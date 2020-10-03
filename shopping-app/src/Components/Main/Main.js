2bec8a1e9972c06f2f6da8096e4dd38178366b36
Pratik Bhagat <pbhagat@deloitte.com>
2020-10-03 11:00:25 +0530

    Commit more hard-coded changes for menu dimensions

--- shopping-app/src/Components/Main/Main.js
+++ shopping-app/src/Components/Main/Main.js
@@ -5,14 +5,16 @@ import ProfileMenu from '../Profile/ProfileMenu/ProfileMenu';
 class Main extends Component {
     render() {
         return (
-            <div className="Main">
-                <div className="Main__menu">
-                    Other menu items come over here
-            </div>
+                <div className="Main">
+                    <div className="Main__menu">
+                        Other menu items come over here
+                </div>
                 <div className="Profile__menu">
                     <ProfileMenu />
                 </div>
-                <div className="Main__content"/>
+                <div className="Main__content">
+                    The main content of the page lies over here and you can visit any of the pages from here
+                </div>
             </div>
         );
     }
