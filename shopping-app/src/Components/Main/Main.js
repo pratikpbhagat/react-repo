6d54b433dbde2546c26012b63efbc58454371094
Pratik Bhagat <pbhagat@deloitte.com>
2020-10-03 10:55:48 +0530

    Commit hardcoded menu placements

--- shopping-app/src/Components/Main/Main.js
+++ shopping-app/src/Components/Main/Main.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Main.css';
 import ProfileMenu from '../Profile/ProfileMenu/ProfileMenu';
 
-const main = (props) => (
-    <div className="Main">
-        <div className="Main__menu">
-            Other menu items come over here
-        </div>
-        <div className="Profile__menu">
-            <ProfileMenu />
-        </div>
-        <div className="Main__content">
-            
-        </div>
-    </div>
-);
+class Main extends Component {
+    render() {
+        return (
+            <div className="Main">
+                <div className="Main__menu">
+                    Other menu items come over here
+            </div>
+                <div className="Profile__menu">
+                    <ProfileMenu />
+                </div>
+                <div className="Main__content"/>
+            </div>
+        );
+    }
+}
 
-export default main;
+export default Main;
