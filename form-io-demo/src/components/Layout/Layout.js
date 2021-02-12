d07d07e4701649166591ed8cf28e7f1b6d0fb96b
Pratik Bhagat <pratik.bhagat@cognext.ai>
2021-02-12 17:29:33 +0530

    Commit layout js

--- form-io-demo/src/components/Layout/Layout.js
+++ form-io-demo/src/components/Layout/Layout.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormBuilder } from 'react-formio';
+import { Form } from 'react-formio';
 
 const Layout = () => {
     const type = 'form';
+
+    const [schema, setSchema] = useState();
+
+    const saveForm = (form) => {
+        console.log(form);
+        setSchema(form);
+    }
+
     return (
-        <FormBuilder form={{ display: type }} onChange={(schema) => console.log(schema)} />
+        <>
+            <FormBuilder form={{ display: type }} onChange={(form) => saveForm(form)} />
+            {/* Form Renderer
+            <Form src={schema} onSubmit={console.log} /> */}
+        </>
     );
 }
 
